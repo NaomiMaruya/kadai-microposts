5b9e6c045900b3b1810c01c6725e0adacc073e43
naomi.M <naomi-maruya@dac.co.jp>
2020-06-11 08:03:14 +0000

    btn-outline

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
     @if (Auth::user()->added_favorites($micropost->id))
         {{-- アンフェイバリッドボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-outline-secondary btn-block btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {{-- フェイバリッドボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-outline-info btn-block btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
--- resources/views/favorite/favorite_card.blade.php
+++ resources/views/favorite/favorite_card.blade.php
@@ -1,21 +1,21 @@
 <div class="d-flex flex-row">
 <div>
- @if (Auth::id() == $favorite->user_id)
-    {{-- 投稿削除ボタンのフォーム --}}
-    {!! Form::open(['route' => ['microposts.destroy', $favorite->id], 'method' => 'delete']) !!}
-    {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-    {!! Form::close() !!}
-@endif
+    @if (Auth::id() == $favorite->user_id)
+        {{-- 投稿削除ボタンのフォーム --}}
+        {!! Form::open(['route' => ['microposts.destroy', $favorite->id], 'method' => 'delete']) !!}
+        {!! Form::submit('Delete', ['class' => 'btn btn-outline-danger btn-sm']) !!}
+        {!! Form::close() !!}
+    @endif
 </div>
     @if (Auth::user()->added_favorites($favorite->id))
         {{-- アンフェイバリッドボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.unfavorite', $favorite->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-block"]) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-outline-secondary btn-block btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {{-- フェイバリッドボタンのフォーム --}}
         {!! Form::open(['route' => ['favorites.favorite', $favorite->id]]) !!}
-            {!! Form::submit('Favorite', ['class' => "btn btn-light btn-block"]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-outline-info btn-block btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
 </div>
